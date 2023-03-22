f7cade073bd19bff136c4ec74725084a8345769c
endlessorhub <anthonyhutchi99@gmail.com>
2023-03-23 01:07:09 +0500

    Implemented SignUp Screen

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import UserDashboard from './Pages/UserDashboard/UserDashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import SignIn from './Pages/SignIn/SignIn';
+import SignUp from './Pages/SignUp/SignUp';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         {/* <UserDashboard /> */}
-        <SignIn />
+        {/* <SignIn /> */}
+        <SignUp />
       </div>
     </BrowserRouter>
   );
