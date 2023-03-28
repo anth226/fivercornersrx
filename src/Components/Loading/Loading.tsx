c83507e2c1a03a17c1313f4692ae88a4b7cdc058
wasiq abdullah <chwasiq0569>
2023-03-29 03:56:20 +0500

    merged changes

--- src/Components/Loading/Loading.tsx
+++ src/Components/Loading/Loading.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { Oval } from 'react-loader-spinner'
+
 const Loading: React.FC = () => {
-    return (<h1>Loading...</h1>);
+  return (
+    <div style={{ width: '100vw', margin: "4rem auto", display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Oval
+        height={50}
+        width={50}
+        color="#0F172A"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel='oval-loading'
+        secondaryColor="#4fa94d"
+        strokeWidth={2}
+        strokeWidthSecondary={2}
+      />
+    </div>);
 }
 
 export default Loading;
